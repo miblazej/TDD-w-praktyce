15addfefbc8e56481419f17f4fcbc258d0aeff22
miblazej <michalblazej@gmail.com>
2021-10-19 22:36:13 +0200

    Rozdział 2 zakończony

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,19 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get("http://moja-witryna.pl:8000")
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Congratulations' in browser.title
+  def setUp(self):
+    self.browser = webdriver.Chrome()
+    self.browser.implicitly_wait(3)
+
+  def tearDown(self):
+    self.browser.quit()
+
+  def test_can_start_a_list_and_retrieve_it_later(self):
+    self.browser.get("http://moja-witryna.pl:8000")
+    self.assertIs('Listy', self.browser.title)
+    self.fail('Zakonczenie testu')
+
+if __name__ == '__main__':
+  unittest.main(warnings='ignore')
