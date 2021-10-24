fbb3c5e6d4b56407c5eae411877b335a858a6ad2
miblazej <michalblazej@gmail.com>
2021-10-24 12:49:53 +0200

    Po refaktoryzacji

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -12,6 +12,13 @@ class NewVisitorTest(unittest.TestCase):
   def tearDown(self):
     self.browser.quit()
 
+  def check_for_row_in_list_table(self, row_text):
+    table = self.browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    self.assertIn(row_text,[row.text for row in rows])
+
+    
+
   def test_can_start_a_list_and_retrieve_it_later(self):
     self.browser.get("http://moja-witryna.pl:8000")
     self.assertIn('Listy', self.browser.title)
@@ -24,10 +31,8 @@ class NewVisitorTest(unittest.TestCase):
     inputbox = self.browser.find_element_by_id('id_new_item')
     inputbox.send_keys('Użyc pawich piór do zrobienia przynęty')
     inputbox.send_keys(Keys.ENTER)
-    table = self.browser.find_element_by_id('id_list_table')
-    rows = table.find_elements_by_tag_name('tr')
-    self.assertIn('1: Kupić pawie pióra', [row.text for row in rows])
-    self.assertIn('2: Użyc pawich piór do zrobienia przynęty', [row.text for row in rows])
+    self.check_for_row_in_list_table('1: Kupić pawie pióra')
+    self.check_for_row_in_list_table('2: Użyc pawich piór do zrobienia przynęty')
     self.fail('Zakonczenie testu')
     
 
