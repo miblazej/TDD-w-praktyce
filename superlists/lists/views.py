324c9af26fcc558ae56a83986095d2e9e4d839d6
miblazej <michalblazej@gmail.com>
2021-10-24 13:42:20 +0200

    Przekierowanie po żadaniu POSt, wyświetlenie wszystkich elementów w szablonie

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,12 +1,16 @@
 from django.http.request import HttpRequest
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from lists.models import Item
 
 # Create your views here.
 
 @csrf_exempt
 def home_page(request):
-  return render(request, 'home.html',{
-    'new_item_text': request.POST.get('item_text',''),
-  })
+  if request.method == 'POST':
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/')
+    
+  items = Item.objects.all()
+  return render(request, 'home.html', {'items': items})
