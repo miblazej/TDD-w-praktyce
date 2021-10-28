a56aaba38d256c7da8d0750128b4b4bc185c92ac
miblazej <michalblazej@gmail.com>
2021-10-28 06:43:00 +0200

    TypeError: byte indices must be integers or slices, not str do naprawienia

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -48,20 +48,33 @@ class ListAndItemModelsTest(TestCase):
 
 class ListViewTest(TestCase):
 
-  def test_home_page_displays_all_list_items(self):
+  def test_uses_list_template(self):
     list_ = List.objects.create()
-    Item.objects.create(text='itemey 1', list=list_)
-    Item.objects.create(text='itemey 2', list=list_)
+    response = self.client.get('/lists/%d/' % (list_.id,))
+    self.assertTemplateUsed(response, 'list.html')  
 
-    response = self.client.get('/lists/the-only-list-in-the-world/')
+  def test_home_page_displays_all_list_items(self):
+    correct_list = List.objects.create()
+    Item.objects.create(text='itemey 1', list=correct_list)
+    Item.objects.create(text='itemey 2', list=correct_list)
+    other_list = List.objects.create()
+    Item.objects.create(text="Element pierwszy innej listy", list=other_list)
+    Item.objects.create(text="Element drugi innej listy", list=other_list)
+
+    response = self.client.get('/lists/%d/' % (correct_list.id,))
 
     self.assertContains(response, 'itemey 1')
     self.assertContains(response, 'itemey 2')
-
-  def test_uses_list_template(self):
-    response = self.client.get('/lists/the-only-list-in-the-world/')
-    self.assertTemplateUsed(response, 'list.html')
+    self.assertNotContains(response, 'Element pierwszy innej listy')
+    self.assertNotContains(response, 'Element drugi innej listy')
   
+  def test_passes_correct_list_to_template(self):
+    other_list = List.objects.create()
+    correct_list = List.objects.create()
+    response = self.client.get('/lists/%d/' % (correct_list.id,))
+    print("test")
+    print(response)
+    self.assertEqual(response.content['list'], correct_list)
 
 class NewListTest(TestCase):
 
@@ -72,5 +85,24 @@ class NewListTest(TestCase):
     self.assertEqual(new_item.text, 'Nowy element listy')
   
   def test_redirects_after_POST(self):
-    response = self.client.post('/lists/new', data={'item_text':'Nowy element listy'})
-    self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+    response = self.client.post('/lists/new', data = {'item_text':'Nowy element listy'})
+    new_list = List.objects.first()
+    self.assertRedirects(response, '/lists/%d/' % (new_list.id))
+
+  def test_can_save_a_POST_request_to_an_existing_list(self):
+    other_list = List.objects.create()
+    correct_list = List.objects.create()
+
+    self.client.post('/lists/%d/add_item' % (correct_list.id,), data={'item_text':'Nowy element dla istniejącej listy'})
+
+    self.assertEqual(Item.objects.count(), 1)
+    new_item = Item.objects.first()
+    self.assertEqual(new_item.text, 'Nowy element dla istniejącej listy')
+    self.assertEqual(new_item.list, correct_list)
+  
+  def test_redirects_to_list_view(self):
+    other_list = List.objects.create()
+    correct_list = List.objects.create()
+
+    response = self.client.post('/lists/%d/add_item' % (correct_list.id,), data={'item_text': 'Nowy element dla istniejącej listy'})
+    self.assertRedirects(response, '/lists/%d/' %(correct_list.id,))
