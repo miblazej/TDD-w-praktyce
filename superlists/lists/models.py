6c96019b85eed26dd92dd48a8134bf2e82fb7f10
miblazej <michalblazej@gmail.com>
2021-10-28 05:44:30 +0200

    Trzy pliki zmienione i dwie migracje

--- superlists/lists/models.py
+++ superlists/lists/models.py
@@ -1,10 +1,16 @@
 from typing import Text
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+class List(models.Model):
+  pass
 
 class Item(models.Model):
   text = models.TextField(default='')
+  list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
 
   def __str__(self):
     return self.text
+
+
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -2,7 +2,7 @@ from django.http.request import HttpRequest
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your views here.
 
@@ -17,5 +17,6 @@ def view_list(request):
 
 @csrf_exempt
 def new_list(request):
-  Item.objects.create(text=request.POST['item_text'])
+  list_ = List.objects.create()
+  Item.objects.create(text=request.POST['item_text'], list=list_)
   return redirect('/lists/the-only-list-in-the-world/')
