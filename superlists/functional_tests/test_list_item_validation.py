83eee8ef9da5c4deb5dd69ed3828023fdadb13bc
miblazej <michalblazej@gmail.com>
2021-11-11 08:29:41 +0100

    Zmienona funckja testów funkcjonalnych.

--- superlists/functional_tests/test_list_item_validation.py
+++ superlists/functional_tests/test_list_item_validation.py
@@ -2,5 +2,21 @@ from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
   def test_cannot_add_empty_list(self):
+    self.browser.get(self.server_url)
+    self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
-    self.fail("Napisz mnie")
+    error = self.browser.find_elements_by_css_selector('.has-error')
+    self.assertEqual(error.text, "Element nie może być pusty")
+
+    self.browser.find_element_by_id('id_new_item').send_keys('Kupić mleko\n')
+    self.check_for_row_in_list_table('1: Kupić mleko')
+
+    self.browser.find_element_by_id('id_new_item').send_keys('\n')
+
+    self.check_for_row_in_list_table('1: Kupić mleko')
+    error = self.browser.find_elements_by_css_selector('.has-error')
+    self.assertEqual(error.text, "Element nie może być pusty")
+
+    self.browser.find_element_by_id('id_new_item').send_keys('Zrobić herbatę\n')
+    self.check_for_row_in_list_table('1: Kupić mleko')
+    self.check_for_row_in_list_table('2: Zrobić herbatę')
