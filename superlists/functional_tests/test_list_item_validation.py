ea9c8262d4920598ac351f8d9ca3549c739ccbcc
miblazej <michalblazej@gmail.com>
2021-11-12 04:38:40 +0100

    Refaktoryzacja widoku listy

--- superlists/functional_tests/test_list_item_validation.py
+++ superlists/functional_tests/test_list_item_validation.py
@@ -1,11 +1,12 @@
 from .base import FunctionalTest
+from selenium.webdriver.common.by import By
 
 class ItemValidationTest(FunctionalTest):
   def test_cannot_add_empty_list(self):
     self.browser.get(self.server_url)
     self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
-    error = self.browser.find_elements_by_css_selector('.has-error')
+    error = self.browser.find_element(By.CSS_SELECTOR,'.has-error')
     self.assertEqual(error.text, "Element nie może być pusty")
 
     self.browser.find_element_by_id('id_new_item').send_keys('Kupić mleko\n')
@@ -14,7 +15,7 @@ class ItemValidationTest(FunctionalTest):
     self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
     self.check_for_row_in_list_table('1: Kupić mleko')
-    error = self.browser.find_elements_by_css_selector('.has-error')
+    error = self.browser.find_element(By.CSS_SELECTOR,'.has-error')
     self.assertEqual(error.text, "Element nie może być pusty")
 
     self.browser.find_element_by_id('id_new_item').send_keys('Zrobić herbatę\n')
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -14,6 +14,9 @@ def home_page(request):
 @csrf_exempt
 def view_list(request, list_id):
   list_ = List.objects.get(id=list_id)
+  if request.method == 'POST':
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/%d/' % (list_.id,))
   return render(request, 'list.html', {'list':list_})
 
 @csrf_exempt
@@ -29,8 +32,3 @@ def new_list(request):
     return render(request, 'home.html',{"error":error})
   return redirect('/lists/%d/' % (list_.id))
 
-@csrf_exempt
-def add_item(request, list_id):
-  list_ = List.objects.get(id=list_id)
-  Item.objects.create(text=request.POST['item_text'], list=list_)
-  return redirect('/lists/%d/' % (list_.id,))
